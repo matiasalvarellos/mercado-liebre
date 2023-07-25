5abe5197404b69e5fcd76621e03d8876049b632c
matias <matias.alvarellos@gmail.com>
2023-07-25 18:21:38 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+//rutas
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "views/home.html"));
+});
+
+app.listen(3000, () => {
+  console.log("Success");
+});
